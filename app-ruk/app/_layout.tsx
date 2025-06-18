f946ac44059fd0d5f73056ca5aa64e8d288daf95
ltin0 <leo_tino@outlook.com.br>
2025-06-18 17:44:50 -0300

    tentado arrumar erro de tema não encontrado no deploy do render

--- app-ruk/app/_layout.tsx
+++ app-ruk/app/_layout.tsx
@@ -1,38 +1,16 @@
 import { ApolloProvider } from '@apollo/client';
 import { Stack } from 'expo-router';
 import client from '../src/lib/apollo';
-import "../global.css"; // Importa o CSS global
+import "../global.css";
 
 export default function RootLayout() {
   return (
     <ApolloProvider client={client}>
-      
-      {/* O componente <Stack> que faltava. Ele é o navegador. */}
       <Stack>
-
-        {/* As configurações de cada tela devem vir aqui dentro */}
-        <Stack.Screen 
-          name="index" // Tela de Login
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="home" // Tela Home
-          options={{ 
-            headerShown: true, 
-            title: 'Meu Perfil' 
-          }} 
-        />
-        <Stack.Screen 
-          name="signup" // Tela de Cadastro
-          options={{ 
-            headerShown: true, 
-            title: 'Criar Nova Conta',
-            presentation: 'modal' 
-          }} 
-        />
-
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="home" options={{ headerShown: true, title: 'Meu Perfil' }} />
+        <Stack.Screen name="signup" options={{ headerShown: true, title: 'Criar Nova Conta', presentation: 'modal' }} />
       </Stack>
-
     </ApolloProvider>
   );
 }
