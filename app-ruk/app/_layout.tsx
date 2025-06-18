a0cc0a386ba49fd77ad090970bac87d7674e5498
ltin0 <leo_tino@outlook.com.br>
2025-06-18 17:35:22 -0300

    ajuste na estrutura do layout para o deploy

--- app-ruk/app/_layout.tsx
+++ app-ruk/app/_layout.tsx
@@ -1,32 +1,38 @@
 import { ApolloProvider } from '@apollo/client';
 import { Stack } from 'expo-router';
 import client from '../src/lib/apollo';
-import "../global.css";
+import "../global.css"; // Importa o CSS global
 
 export default function RootLayout() {
   return (
     <ApolloProvider client={client}>
+      
+      {/* O componente <Stack> que faltava. Ele é o navegador. */}
       <Stack>
+
+        {/* As configurações de cada tela devem vir aqui dentro */}
         <Stack.Screen 
-          name="index"
-          options={{ headerShown: false }}
+          name="index" // Tela de Login
+          options={{ headerShown: false }} 
         />
         <Stack.Screen 
-          name="home"
+          name="home" // Tela Home
           options={{ 
             headerShown: true, 
             title: 'Meu Perfil' 
           }} 
         />
         <Stack.Screen 
-          name="signup" 
+          name="signup" // Tela de Cadastro
           options={{ 
             headerShown: true, 
             title: 'Criar Nova Conta',
             presentation: 'modal' 
           }} 
         />
+
       </Stack>
+
     </ApolloProvider>
   );
 }
